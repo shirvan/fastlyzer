a0ba8b5cdd67f8b5678fb3dc73b8bbb348cc384c
Korrat <korrat@protonmail.com>
2019-10-03 11:11:23 +0200

    Filter directory entries from list of files to read

--- src/rw.rs
+++ src/rw.rs
@@ -1,21 +1,20 @@
 use crate::FastResult;
+use concat_reader::concat_path;
 use std::{
     fs::OpenOptions,
     io::{BufRead, BufReader},
     path::Path,
 };
-use concat_reader::concat_path;
 
 pub fn reader(file: &str) -> FastResult<Box<dyn BufRead>> {
     let path = Path::new(file);
     if path.is_dir() {
-        let paths = path.read_dir()
+        let paths = path
+            .read_dir()
             .into_iter()
-            .map(|file| {
-                file.map(|f| f.unwrap().path())
-                    .collect::<Vec<_>>()
-            })
+            .map(|file| file.map(|f| f.unwrap().path()).collect::<Vec<_>>())
             .flatten()
+            .filter(|path| path.is_file())
             .collect::<Vec<_>>();
         let file = concat_path(paths);
         let buff = BufReader::new(file);
